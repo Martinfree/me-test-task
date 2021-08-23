25420873115e966f85f2a54e0152fb49bda2e389
Martinfree <lvarchenko99@gmail.com>
2021-08-23 22:56:52 +0300

    Initialize Blazor App, write Class TaskModel & WorkerModel, write first controller with some pages

---
+++ me-task-blazor/me-task-blazor/Shared/Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace me_task_blazor.Shared
+{
+    public class TaskModel
+    {
+
+        public int Id { get; set; }
+        public int Images { get; set; }
+        //OneToMany relation. Represent all workers that attented in task
+        private ICollection<WorkerModel> Workers { get; set; }
+        //???
+        public DateTime ReleaseDate { get; set; }
+        public string Res { get; set; }
+   
+    }
+}
---
+++ me-task-blazor/me-task-blazor/Shared/Worker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace me_task_blazor.Shared
+{
+    public class WorkerModel
+{
+    public int Id { get; set; } //???
+    public string Name { get; set; }
+    public double ImgPerMinute { get; set; }
+}
+}
