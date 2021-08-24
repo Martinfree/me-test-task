c70b2ff335b6be6d3aa2120c6aa898eb9ae4de2b
Леонід Варченко <lvarchenko99@gmail.com>
2021-08-24 19:45:02 +0300

    Change models

--- me-task-blazor/me-task-blazor/Shared/TaskModel.cs
+++ me-task-blazor/me-task-blazor/Shared/TaskModel.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using me_task_blazor.Shared;
 
 namespace me_task_blazor.Shared
 {
     public class TaskModel
     {
-
+        [Required]
+        [Key]
         public int Id { get; set; }
         public int Images { get; set; }
         //OneToMany relation. Represent all workers that attented in task
-        private ICollection<WorkerModel> Workers { get; set; }
+        public ICollection<WorkerModel> Workers { get; set; }
         //???
+        [DataType(DataType.DateTime)]
         public DateTime ReleaseDate { get; set; }
         public string Res { get; set; }
-   
+
     }
 }
---
+++ me-task-blazor/me-task-blazor/Shared/WorkerModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace me_task_blazor.Shared
+{
+    public class WorkerModel
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; } //???
+        public string Name { get; set; }
+        public int ImgPerMinute { get; set; }
+    }
+}
