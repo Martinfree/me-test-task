121bdcfaf6257c1966f42bfd441405cc354b314d
Леонід Варченко <lvarchenko99@gmail.com>
2021-08-24 19:47:59 +0300

    Write new api controller for TaskModel with DataContext

---
+++ me-task-blazor/me-task-blazor/Server/Controllers/TaskModelsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using me_task_blazor.Server.Models;
+using me_task_blazor.Shared;
+
+
+namespace me_task_blazor.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskModelsController : ControllerBase
+    {
+        DataContext db;
+
+        public TaskModelsController(DataContext context)
+        {
+            db = context;
+            if (!db.TaskModels.Any())
+            {
+                db.TaskModels.Add(new TaskModel { });
+                db.TaskModels.Add(new TaskModel { });
+                db.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskModel>>> Get()
+        {
+            return await db.TaskModels.ToListAsync();
+        }
+
+        // GET api/TaskModels/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskModel>> Get(int id)
+        {
+            TaskModel TaskModel = await db.TaskModels.FirstOrDefaultAsync(x => x.Id == id);
+            if (TaskModel == null)
+                return NotFound();
+            return new ObjectResult(TaskModel);
+        }
+
+        // POST api/TaskModels/
+        [HttpPost]
+        public async Task<ActionResult<TaskModel>> Post(TaskModel TaskModel)
+        {
+            if (TaskModel == null)
+            {
+                return BadRequest();
+            }
+
+            db.TaskModels.Add(TaskModel);
+            await db.SaveChangesAsync();
+            return Ok(TaskModel);
+        }
+    }
+}
